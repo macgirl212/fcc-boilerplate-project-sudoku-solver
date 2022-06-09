d5fb417968f3340c91bb2122f1d1f4b790f9bb64
Melody Davis <piano.fan122@gmail.com>
2022-06-09 15:03:33 -0400

    added more error messages

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,42 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 module.exports = function (app) {
 	let solver = new SudokuSolver();
 
-	app.route('/api/check').post((req, res) => {});
+	app.route('/api/check').post((req, res) => {
+		const { puzzle, coordinate, value } = req.body;
+
+		// check if all fields are filled in
+		if (!puzzle || !coordinate || !value) {
+			return res.json({ error: 'Required field(s) missing' });
+		}
+
+		// puzzle validator
+		const validate = solver.validate(puzzle);
+		if (validate instanceof Object && validate.hasOwnProperty('error')) {
+			return res.json(solver.validate(puzzle));
+		}
+
+		// coordinate validator
+		if (!coordinate.match(/^[a-iA-I][1-9]$/)) {
+			return res.json({ error: 'Invalid coordinate' });
+		}
+
+		// value validator
+		if (!value.match(/^[1-9]$/)) {
+			return res.json({ error: 'Invalid value' });
+		}
+
+		const [row, column] = coordinate.split('');
+
+		return res.json({ msg: 'checked' });
+	});
 
 	app.route('/api/solve').post((req, res) => {
 		const puzzle = req.body.puzzle;
 
+		const validate = solver.validate(puzzle);
+
 		// error validator
-		if (solver.validate(puzzle) instanceof Object) {
+		if (validate instanceof Object && validate.hasOwnProperty('error')) {
 			return res.json(solver.validate(puzzle));
 		}
 		console.log('solved');
